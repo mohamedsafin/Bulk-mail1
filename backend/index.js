abb723f79c7aaff95edde4257f207345cb8c5bd4
mohamedsafin <mohamedsafin2004@gmail.com>
2026-03-22 23:09:11 +0530

    deploy backend

--- backend/index.js
+++ backend/index.js
@@ -92,6 +92,7 @@ app.post("/sendmail", async function (req, res) {
 });
 
 // Server
-app.listen(5000, function () {
-  console.log("Server is running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, function () {
+  console.log("Server is running");
 });
